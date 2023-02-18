7ba7c0594c516ccd985d2bb7ed51622de7e775af
Juan López <juanem@hotmail.com>
2023-02-17 22:45:33 -0300

    clean code

--- src/main/java/com/complejolapasionaria/reservation/repository/ICommerceBuildingRepository.java
+++ src/main/java/com/complejolapasionaria/reservation/repository/ICommerceBuildingRepository.java
@@ -7,14 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ICommerceBuildingRepository extends JpaRepository<CommerceBuilding,Long> {
     boolean existsByName(String name);
     boolean existsByIdAndOwner(Long id, User user);
     boolean existsById(Long id);
-    List<CommerceBuilding> findAllByOwner(User owner);
     Page<CommerceBuilding> findAllByDeleted(boolean deleted, Pageable page);
     CommerceBuilding getReferenceByIdAndOwner(Long id, User user);
 }
--- src/main/java/com/complejolapasionaria/reservation/repository/IRentalUnitRepository.java
+++ src/main/java/com/complejolapasionaria/reservation/repository/IRentalUnitRepository.java
@@ -7,11 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface IRentalUnitRepository extends JpaRepository<RentalUnit,Long> {
     boolean existsByNameAndBuilding(String name, CommerceBuilding id);
-    boolean existsByIdAndBuildingIn(Long id, List<CommerceBuilding> buildingList);
     Page<RentalUnit> findAllByDeleted(boolean deleted, Pageable page);
 }
