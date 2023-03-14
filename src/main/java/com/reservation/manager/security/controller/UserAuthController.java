4d93770d1ec83355e624c9f5fafb949c2a0303a5
Juan López <juanem@hotmail.com>
2023-03-14 16:20:16 -0300

    add swagger doc in auth controller

--- src/main/java/com/reservation/manager/security/controller/UserAuthController.java
+++ src/main/java/com/reservation/manager/security/controller/UserAuthController.java
@@ -5,6 +5,10 @@ import com.reservation.manager.dto.auth.AuthenticationResponseDto;
 import com.reservation.manager.dto.RequestUserDto;
 import com.reservation.manager.dto.AuthRegisterResponseDto;
 import com.reservation.manager.security.service.IAuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,11 +27,21 @@ public class UserAuthController extends
 
     private final IAuthenticationService iAuthenticationService;
 
+    @Operation(method = "POST", summary = "Application register", description = "Get all user info",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Account registered", content = @Content(
+                            mediaType = "application/json", schema = @Schema(implementation = AuthRegisterResponseDto.class)))
+            })
     @PostMapping("/register")
     public ResponseEntity<AuthRegisterResponseDto> signUp(@Valid @RequestBody RequestUserDto dto) throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED).body(iAuthenticationService.saveUser(dto));
     }
 
+    @Operation(method = "POST", summary = "signIn", description = "Get jwt token necessary for access",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Account logged", content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = AuthenticationResponseDto.class)))
+            })
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponseDto> signIn(@Valid @RequestBody AuthenticationRequestUserDto dto) throws Exception {
         return ResponseEntity.status(HttpStatus.OK).body(iAuthenticationService.authenticate(dto));
