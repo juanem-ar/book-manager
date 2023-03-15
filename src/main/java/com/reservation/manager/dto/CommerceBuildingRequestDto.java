0ef6473db675ab2697cead5d63695711c251f07b
Juan López <juanem@hotmail.com>
2023-03-15 14:05:45 -0300

    patter regexp added into commercer request dto

--- src/main/java/com/reservation/manager/dto/CommerceBuildingRequestDto.java
+++ src/main/java/com/reservation/manager/dto/CommerceBuildingRequestDto.java
@@ -3,6 +3,7 @@ package com.reservation.manager.dto;
 import com.reservation.manager.model.RentalUnit;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,6 +24,8 @@ public class CommerceBuildingRequestDto {
     @NotNull(message = "commerce building's address is required.")
     private String address;
 
+    @NotNull(message = "phone number is required.")
+    @Pattern(regexp = "^[+][1-9]{2,3}[1-9]\\d{8,9}$", message = "Format: (+) + Area code + phone number. Without spaces and special characters")
     private String phoneNumber;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
